3c39357b45e2762d62e5e6613b64abe56383770d
frfinam <40425345+frfinam@users.noreply.github.com>
2023-06-20 18:52:01 +0200

    Update RDCG_Steel_Map_app.py

--- RDCG_Steel_Map_app.py
+++ RDCG_Steel_Map_app.py
@@ -31,7 +31,7 @@ def create_onedrive_directdownload (onedrive_link):
 
 #Connect to rddump of easystructure dataview to have prefabrication progress
 cols   = ['Area', 'Structure', 'dp_mp', 'Name', 'RD', 'am_serial_no', 'Weight', 'DESCRIP', 'Vendor', 'fab_start_date', 'fab_completed_date', 'srn_date']
-df_one_drive = "https://1drv.ms/u/s!AiiyfzN3UvpehmUkUFsbpinRg_IV?e=gLQ7Sm"
+df_one_drive = "https://1drv.ms/u/s!AiiyfzN3UvpehmtMgdLsh8aMt813?e=QyoUBi"
 df_link = create_onedrive_directdownload (df_one_drive)
 df = pd.read_csv(df_link, encoding='ISO-8859-1')[cols]
 
@@ -198,7 +198,7 @@ geodf['Long']=geodf['geometry'].x
 # open the list of tags
 #df_Tag_List = pd.read_csv("C://Users//ffinamore//Desktop//Folium//Input//MainStructures.csv"
 # open the list of tags option from one drive
-df_Tag_List_one_drive = "https://1drv.ms/x/s!AiiyfzN3UvpehmQYM6quS_oQA5Vv?e=71WiqS"
+df_Tag_List_one_drive = "https://1drv.ms/u/s!AiiyfzN3UvpehmlCvqma5PczBH-o?e=AZGery"
 df_Tag_List_one_link = create_onedrive_directdownload (df_Tag_List_one_drive)
 df_Tag_List = pd.read_csv(df_Tag_List_one_link,encoding='ISO-8859-1')
 
